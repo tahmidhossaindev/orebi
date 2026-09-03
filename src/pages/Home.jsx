4ac4902256c990656fe678f7ab81fc5dc378c2cf
tahmidhossaindev <tahmidhossain345@gmail.com>
2026-09-04 00:46:20 +0600

    home update two

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,81 @@
 import React from 'react'
 import Banner from '../layouts/Banner'
 import Container from '@/components/Container'
+import { FaTruck } from "react-icons/fa";
+import { MdRefresh } from "react-icons/md";
+import Flex from '@/components/Flex';
+import Image from '@/components/Common/Image';
+import adOne from '/src/assets/adOne.jpg'
+import adTwo from '/src/assets/adTwo.png'
+import adThree from '/src/assets/adThree.jpg'
+import longAd from '/src/assets/longAd.png'
 
 const Home = () => {
   return (
     <>
     <Banner/>
+
+{/* -----------Points part */}
+
+
     <div className="bg-white border-2 border-[#F0F0F0] py-7">
       <Container>
-      <div className="flex justify-between">
+      <div className="flex justify-between font-orebi">
 
-        <div className="flex gap-x-3.75">
-          <h3>2</h3>
-          <p>Two years warranty</p>
+        <div className="flex gap-x-3 items-center">
+          <h3 className='text-[#262626] font-bold text-[24px]'>2</h3>
+          <p className='text-[16px] text-[#6D6D6D]'>Two years warranty</p>
         </div>
        
-        <div className="flex gap-x-3.75">
-          <h3>2</h3>
-          <p>Two years warranty</p>
+        <div className="flex gap-x-3 items-center">
+          <FaTruck className='text-[#262626] font-bold text-[24px]' />
+          <p className='text-[16px] text-[#6D6D6D]'>Free shipping</p>
         </div>
        
-        <div className="flex gap-x-3.75">
-          <h3>2</h3>
-          <p>Two years warranty</p>
+        <div className="flex gap-x-2.5 items-center">
+          <MdRefresh className='text-[#262626] font-bold text-[24px]' />
+          <p className='text-[16px] text-[#6D6D6D]'>Return policy in 30 days</p>
         </div>
 
 
       </div>
       </Container>
     </div>
+
+{/* sales part  */}
+  
+
+<Container className={"pt-35"}>
+  <Flex className={"gap-x-10"}>
+
+    <div className="">
+      <Image imageSrc={adOne} className={"w-full"}/>
+    </div>    
+
+    <div className="">
+      <Image imageSrc={adTwo} className={"w-full mb-8.75"}/>
+      <Image imageSrc={adThree} className={"w-full"}/>
+    </div>
+
+  </Flex>
+</Container>
+
+
+{/* New arrivals */}
+
+
+
+
+{/* Long Ad  */}
+<Container>
+
+<div className="py-35">
+  <Image imageSrc={longAd}/>
+</div>
+
+</Container>
+
+
     </>
   )
 }
