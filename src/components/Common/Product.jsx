a3ebd451a365eac7d94a9807bfa0a5adfb21e5f5
tahmidhossaindev <tahmidhossain345@gmail.com>
2026-09-04 02:00:53 +0600

    home update five

--- src/components/Common/Product.jsx
+++ src/components/Common/Product.jsx
@@ -1,20 +1,53 @@
 import React from 'react'
 import Image from './Image'
 import Badge from './Badge'
+import { FaShoppingCart } from "react-icons/fa";
+import Flex from '../Flex'
+import { FaHeart } from "react-icons/fa";
+import { LuRefreshCcw } from "react-icons/lu";
 
 const Product = ({productImage, badgeT, productName, productPrice, productCategory}) => {
   return (
   <>
-  <div className="relative mb-5.5">
+
+
+  <div className="relative mb-5.5 group">
     <Image imageSrc={productImage}/>
     <Badge badgeText={`${badgeT}`} className={"absolute top-3 left-3"}/>
-   
+
+    {/* hover part  */}
+   <div className="absolute hidden bg-white group-hover:block bottom-0 left-0 w-full duration-150 ease-in-out pr-7 py-6 font-orebi">
+  
+        <div className="flex gap-x-2 justify-end space-y-6">
+            <h3 className=' text-[#767676] text-[16px]' >Add to Wishlist</h3>
+            <FaHeart className='font-bold text-[#262626] text-[16px]'/>
+        </div>
+        
+          <div className="flex gap-x-2 justify-end space-y-6">
+            <h3 className=' text-[#767676] text-[16px]'>Compare</h3>
+            <LuRefreshCcw className='font-bold text-[#262626] text-[16px]'/>
+        </div>
+       
+          <div className="flex gap-x-2 justify-end space-y-6">
+            <h3 className='font-bold text-[#262626] text-[16px]'>Add to Cart</h3>
+            <FaShoppingCart className='font-bold text-[#262626] text-[16px]' />
+        </div>
+
+
+
+   </div>
+
+
   </div>
+  
+{/* textpart  */}
+
    <div className="flex justify-between font-orebi mb-3.75">
         <h3 className='font-bold text-[20px] text-[#262626]'>{productName}</h3>
         <p className='text-[16px] text-[#767676] height-[30px]'>{productPrice}</p>
     </div>
     <p className='text-[16px] text-[#767676] height-[30px]'>{productCategory}</p>
+
 
   </>
   )
