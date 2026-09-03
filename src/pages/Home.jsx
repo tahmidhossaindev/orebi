1bf48d4bf7a176de25a9077030b751babbee6836
tahmidhossaindev <tahmidhossain345@gmail.com>
2026-09-04 00:15:43 +0600

    home update one

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import Banner from '../layouts/Banner'
+import Container from '@/components/Container'
 
 const Home = () => {
   return (
     <>
     <Banner/>
-    <div >work in progress</div>
+    <div className="bg-white border-2 border-[#F0F0F0] py-7">
+      <Container>
+      <div className="flex justify-between">
+
+        <div className="flex gap-x-3.75">
+          <h3>2</h3>
+          <p>Two years warranty</p>
+        </div>
+       
+        <div className="flex gap-x-3.75">
+          <h3>2</h3>
+          <p>Two years warranty</p>
+        </div>
+       
+        <div className="flex gap-x-3.75">
+          <h3>2</h3>
+          <p>Two years warranty</p>
+        </div>
+
+
+      </div>
+      </Container>
+    </div>
     </>
   )
 }
