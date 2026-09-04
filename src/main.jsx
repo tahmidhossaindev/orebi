92590edaae37b9c9635ef3c13cc74fb6618514d8
tahmidhossaindev <tahmidhossain345@gmail.com>
2026-09-04 15:06:17 +0600

    rawfinish

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import './index.css'
  import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+   
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  
   </StrictMode>
-)
+);
